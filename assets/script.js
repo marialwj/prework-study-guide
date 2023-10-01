fbe8af190e5b824b5a3b10a93dc64bdecf80a2d8
marialwj <144569725+marialwj@users.noreply.github.com>
2023-09-30 19:24:56 -0700

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -21,6 +21,6 @@ function selectTopic() {
     }
   }
 console.log('Here are the topics we learned through Prework');
-listTopics()
+listTopics();
 console.log('Which topic should we study first?')
-selectTopic()
+selectTopic();
